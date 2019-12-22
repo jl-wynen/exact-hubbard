57f588b08693458df77df2db3d532e58fe826b97
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-12-22 15:13:33 +0100

    Split declaration/definition of ostream operators

---
+++ src/io.cpp
@@ -0,0 +1,25 @@
+#include "io.hpp"
+
+
+std::ostream &operator<<(std::ostream &os, PH ph) {
+    switch (ph) {
+        case PH::n:
+            os << ". "; break;
+        case PH::p:
+            os << "p "; break;
+        case PH::h:
+            os << " h"; break;
+        case PH::ph:
+            os << "ph"; break;
+    }
+    return os;
+}
+
+std::ostream &operator<<(std::ostream &os, State const &state) {
+    os << '|';
+    for (std::size_t i = 0; i + 1 < state.size(); ++i) {
+        os << state[i] << ' ';
+    }
+    os << state[state.size()-1] << '>';
+    return os;
+}
--- src/io.hpp
+++ src/io.hpp
@@ -7,29 +7,9 @@
 #include "state.hpp"
 
 
-std::ostream &operator<<(std::ostream &os, PH ph)
-{
-    switch (ph) {
-        case PH::n:
-            os << ". "; break;
-        case PH::p:
-            os << "p "; break;
-        case PH::h:
-            os << " h"; break;
-        case PH::ph:
-            os << "ph"; break;
-    }
-    return os;
-}
+std::ostream &operator<<(std::ostream &os, PH ph);
 
 
-std::ostream &operator<<(std::ostream &os, State const &state) {
-    os << '|';
-    for (std::size_t i = 0; i + 1 < state.size(); ++i) {
-        os << state[i] << ' ';
-    }
-    os << state[state.size()-1] << '>';
-    return os;
-}
+std::ostream &operator<<(std::ostream &os, State const &state);
 
 #endif //EXACT_HUBBARD_IO_HPP
