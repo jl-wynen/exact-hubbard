fdc61de4b9118ca6afbf6fc07cacf6a468e40c76
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-12-23 20:53:46 +0100

    Move Operator implementations into apply_implX functions

--- src/operator.hpp
+++ src/operator.hpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <utility>
 
+#include "always_false.hpp"
 #include "state.hpp"
 
 
@@ -20,11 +21,12 @@ constexpr int countPHBefore(State const &state, std::size_t const site) noexcept
 
 
 template <typename T, typename = void>
-struct hasSingleStateApplyWithOut : std::false_type {};
+struct hasApplyImplSingleOutparam : std::false_type {};
 
 template <typename T>
-struct hasSingleStateApplyWithOut<T,
-        std::void_t<decltype(std::declval<T>().apply(std::declval<SumState>(), std::declval<SumState&>()))>>
+struct hasApplyImplSingleOutparam<T,
+        std::void_t<decltype(std::declval<T>().apply_implSingleOutparam(std::declval<State>(),
+                                                                        std::declval<SumState&>()))>>
         : std::true_type {};
 
 
@@ -48,16 +50,12 @@ struct Operator
 
     void apply(State const &state, SumState &out) const
     {
-        if constexpr (hasSingleStateApplyWithOut<Derived>::value) {
-            // use implementation provided by derived type
-            asDerived().apply(state, out);
+        if constexpr (hasApplyImplSingleOutparam<Derived>::value) {
+            asDerived().apply_implSingleOutparam(state, out);
         }
         else {
-            // use generic implementation
-            auto const[c, s] = asDerived().apply(state);
-            if (c != 0.0) {
-                out.push(c, s);
-            }
+            static_assert(alwaysFalse_v<Derived>,
+                          "The operator class has no valid implementation of apply");
         }
     }
 
@@ -95,18 +93,15 @@ struct ParticleCreator : Operator<ParticleCreator>
     explicit constexpr ParticleCreator(std::size_t const s) noexcept : site{s} { }
 
 
-    using Operator<ParticleCreator>::apply;
-
-
-    [[nodiscard]] constexpr std::pair<double, State> apply(State const &state) const noexcept
+    void apply_implSingleOutparam(State const &state, SumState &out) const
     {
         if (state.hasParticleOn(site)) {
-            return {0, {}};
+            return;  // cannot create where there is a particle already
         }
         State aux{state};
         aux.addParticleOn(site);
-        return {countPHBefore(state, site) % 2 == 0 ? +1.0 : -1.0,
-                aux};
+        out.push(countPHBefore(state, site) % 2 == 0 ? +1.0 : -1.0,
+                 aux);
     }
 };
 
@@ -119,18 +114,15 @@ struct ParticleAnnihilator : Operator<ParticleAnnihilator>
     explicit constexpr ParticleAnnihilator(std::size_t const s) noexcept : site{s} { }
 
 
-    using Operator<ParticleAnnihilator>::apply;
-
-
-    [[nodiscard]] constexpr std::pair<double, State> apply(State const &state) const noexcept
+    void apply_implSingleOutparam(State const &state, SumState &out) const
     {
         if (not state.hasParticleOn(site)) {
-            return {0, {}};
+            return;  // cannot destroy a particle when there is none
         }
         State aux{state};
         aux.removeParticleOn(site);
-        return {countPHBefore(state, site) % 2 == 0 ? +1.0 : -1.0,
-                aux};
+        out.push(countPHBefore(state, site) % 2 == 0 ? +1.0 : -1.0,
+                 aux);
     }
 };
 
@@ -143,19 +135,16 @@ struct HoleCreator : Operator<HoleCreator>
     explicit constexpr HoleCreator(std::size_t const s) noexcept : site{s} { }
 
 
-    using Operator<HoleCreator>::apply;
-
-
-    [[nodiscard]] constexpr std::pair<double, State> apply(State const &state) const noexcept
+    void apply_implSingleOutparam(State const &state, SumState &out) const
     {
         if (state.hasHoleOn(site)) {
-            return {0, {}};
+            return;  // cannot create a hole where there is a hole already
         }
         State aux{state};
         aux.addHoleOn(site);
         auto const nswaps = countPHBefore(state, site) + (state.hasParticleOn(site) ? 1 : 0);
-        return {nswaps % 2 == 0 ? +1.0 : -1.0,
-                aux};
+        out.push(nswaps % 2 == 0 ? +1.0 : -1.0,
+                 aux);
     }
 };
 
@@ -168,19 +157,16 @@ struct HoleAnnihilator : Operator<HoleAnnihilator>
     explicit constexpr HoleAnnihilator(std::size_t const s) noexcept : site{s} { }
 
 
-    using Operator<HoleAnnihilator>::apply;
-
-
-    [[nodiscard]] constexpr std::pair<double, State> apply(State const &state) const noexcept
+    void apply_implSingleOutparam(State const &state, SumState &out) const
     {
         if (not state.hasHoleOn(site)) {
-            return {0, {}};
+            return;  // cannot destroy a hole when there is none
         }
         State aux{state};
         aux.removeHoleOn(site);
         auto const nswaps = countPHBefore(state, site) + (state.hasParticleOn(site) ? 1 : 0);
-        return {nswaps % 2 == 0 ? +1.0 : -1.0,
-                aux};
+        out.push(nswaps % 2 == 0 ? +1.0 : -1.0,
+                 aux);
     }
 };
 
@@ -195,13 +181,10 @@ struct NumberOperator : Operator<NumberOperator>
     explicit constexpr NumberOperator(std::size_t const s) noexcept : site{s} { }
 
 
-    using Operator<NumberOperator>::apply;
-
-
-    [[nodiscard]] constexpr std::pair<double, State> apply(State const &state) const noexcept
+    void apply_implSingleOutparam(State const &state, SumState &out) const
     {
-        return {(state.hasParticleOn(site) ^ state.hasHoleOn(site)) ? 1.0 : 0.0,
-                state};
+        out.push((state.hasParticleOn(site) ^ state.hasHoleOn(site)) ? 1.0 : 0.0,
+                 state);
     }
 };
 
@@ -211,7 +194,7 @@ struct ParticleHop : Operator<ParticleHop>
     using Operator<ParticleHop>::apply;
 
 
-    void apply(State const &state, SumState &out) const
+    void apply_implSingleOutparam(State const &state, SumState &out) const
     {
         for (auto const [a, b] : nearestNeighbours) {
             if (state.hasParticleOn(a) and not state.hasParticleOn(b)) {
@@ -231,9 +214,19 @@ private:
     [[nodiscard]] std::pair<double, State>
     doHop(State const &state, std::size_t const from, std::size_t const to) const noexcept
     {
-        auto const [coefAnnihilate, aux] = ParticleAnnihilator{from}.apply(state);
-        auto const [coefCreate, newState] = ParticleCreator{to}.apply(aux);
-        return {-kappa * coefAnnihilate * coefCreate, newState};
+        // How often does the annihilator have to swap places with another operator?
+        auto const nSwapAnnihilate = countPHBefore(state, from);
+        // Annihilate
+        State newState{state};
+        newState.removeParticleOn(from);
+
+        // How often does the creator have to swap places with another operator?
+        auto const nSwapCreate = countPHBefore(newState, to);
+        // Create
+        newState.addParticleOn(to);
+
+        return {-kappa * ((nSwapAnnihilate+nSwapCreate) % 2 == 0 ? +1.0 : -1.0),
+                newState};
     }
 };
 
@@ -243,7 +236,7 @@ struct HoleHop : Operator<HoleHop>
     using Operator<HoleHop>::apply;
 
 
-    void apply(State const &state, SumState &out) const
+    void apply_implSingleOutparam(State const &state, SumState &out) const
     {
         for (auto const [a, b] : nearestNeighbours) {
             if (state.hasHoleOn(a) and not state.hasHoleOn(b)) {
@@ -263,9 +256,19 @@ private:
     [[nodiscard]] std::pair<double, State>
     doHop(State const &state, std::size_t const from, std::size_t const to) const noexcept
     {
-        auto const [coefAnnihilate, aux] = HoleAnnihilator{from}.apply(state);
-        auto const [coefCreate, newState] = HoleCreator{to}.apply(aux);
-        return {-kappa * coefAnnihilate * coefCreate, newState};
+        // How often does the annihilator have to swap places with another operator?
+        auto const nSwapAnnihilate = countPHBefore(state, from) + (state.hasParticleOn(from) ? 1 : 0);
+        // Annihilate
+        State newState{state};
+        newState.removeHoleOn(from);
+
+        // How often does the creator have to swap places with another operator?
+        auto const nSwapCreate = countPHBefore(newState, to) + (state.hasParticleOn(to) ? 1 : 0);
+        // Create
+        newState.addHoleOn(to);
+
+        return {-kappa * ((nSwapAnnihilate+nSwapCreate) % 2 == 0 ? +1.0 : -1.0),
+                newState};
     }
 };
 
