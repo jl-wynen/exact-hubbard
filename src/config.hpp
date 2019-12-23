e5eaf6c7c691da8f4f8dbfe56accdb60b32f5049
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-12-23 13:38:54 +0100

    Add nearestNeighours List

--- src/config.hpp
+++ src/config.hpp
@@ -1,8 +1,20 @@
 #ifndef EXACT_HUBBARD_CONFIG_HPP
 #define EXACT_HUBBARD_CONFIG_HPP
 
+#include <array>
 #include <cstdint>
+#include <utility>
 
-constexpr std::size_t NSITES = 2;
+
+constexpr static std::size_t NSITES = 3;
+
+
+using Link = std::pair<std::size_t, std::size_t>;
+
+// Triangle
+constexpr static std::array nearestNeighbours
+        = {Link{0, 1},
+           Link{1, 2},
+           Link{2, 0}};
 
 #endif //EXACT_HUBBARD_CONFIG_HPP
