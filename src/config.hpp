96887f458f82b81e940b82f2c18570b932312b32
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-12-23 16:48:31 +0100

    Add kappa, beta, U

--- src/config.hpp
+++ src/config.hpp
@@ -11,10 +11,22 @@ constexpr static std::size_t NSITES = 3;
 
 using Link = std::pair<std::size_t, std::size_t>;
 
+/*
+ * If Link{i, j} is present, Link{j, i} is implicitly assumed to be there as well.
+ */
+
 // Triangle
 constexpr static std::array nearestNeighbours
         = {Link{0, 1},
            Link{1, 2},
            Link{2, 0}};
+
+
+constexpr double kappa = 1.0;
+
+constexpr double beta = 1.0;
+
+constexpr double U = 1.0;
+
 
 #endif //EXACT_HUBBARD_CONFIG_HPP
