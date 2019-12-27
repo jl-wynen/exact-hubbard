0d00f352073f5be2a26e0a02c010104857f9e11c
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2019-12-27 16:38:38 +0100

    Compute spectrum in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 
 #include "state.hpp"
 #include "io.hpp"
 #include "operator.hpp"
+#include "matrix.hpp"
+
+
+template <typename OP>
+void computeSpectrum(OP const &hamiltonian,
+                     SumState const &basis)
+{
+    auto const mat = toMatrix(hamiltonian, basis);
+    std::ofstream ofs("../hamiltonian.mat");
+    ofs << mat << '\n';
+};
 
 
 int main()
 {
     auto const fockspace = fockspaceBasis();
 
-    ParticleCreator ad{1};
-    ParticleAnnihilator a{0};
+    SumOperator hamiltonian{ParticleHop{},
+                            HoleHop{},
+                            SquaredNumberOperator{}};
 
-    auto aux = ad.apply(fockspace);
-    auto out = a.apply(aux);
-
-    for (size_t i = 0; i < fockspace.size(); ++i) {
-        auto [c, s] = fockspace[i];
-        auto [ca, sa] = aux[i];
-        auto [c2, s2] = out[i];
-        std::cout << std::setw(2) << c << ' ' <<  s
-                  << "   " << std::setw(2) << ca << ' ' << sa
-                  << "   " << std::setw(2) << c2 << ' ' << s2 << '\n';
-    }
-    std::cout << fockspace.size() << '\n';
-
+    computeSpectrum(hamiltonian, fockspace);
 }
