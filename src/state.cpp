85864ba99f50a8bf5c8d68b47ce153a8ef05d0d4
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-01-17 11:22:07 +0100

    Document (Sum)State

--- src/state.cpp
+++ src/state.cpp
@@ -4,6 +4,7 @@
 
 
 namespace {
+    /// Erase an elements from a std::vector.
     template <typename T>
     void erase(std::vector<T> &vec, std::size_t const i)
     {
@@ -16,25 +17,29 @@ namespace {
 void SumState::compress()
 {
     for (std::size_t i = 0; i < coefs_.size(); ++i) {
-        // join elements with the same state
+        // Join elements with the same state.
+        // Compare state i with all states j > i.
+        // All states k < i are already unique.
         for (std::size_t j = coefs_.size()-1; j > i; --j) {
             if (states_[i] == states_[j]) {
                 coefs_[i] += coefs_[j];
                 erase(coefs_, j);
                 erase(states_, j);
+                // no need to modify j here as only elements k > j shift when erasing
             }
         }
 
-        // erase elements with coefficient 0
+        // erase elements with coefficient 0 from cancellation
         if (std::abs(coefs_[i]) < 1e-13) {
             erase(coefs_, i);
             erase(states_, i);
-            --i;  // check the new element at this position next
+            --i;  // check the new element at this position next (elements have been shifted)
         }
     }
 }
 
 namespace {
+    // move a PH along the chain n -> p -> h -> ph -> n
     constexpr std::optional<PH> increment(PH ph) noexcept
     {
         if (ph == PH::ph) {
@@ -45,6 +50,7 @@ namespace {
     }
 
 
+    // increment the PH in a state at site dim
     [[nodiscard]] constexpr bool increment(State &state, size_t const dim = 0) noexcept
     {
         auto const inc = increment(state[dim]);
@@ -69,6 +75,7 @@ SumState fockspaceBasis()
     State auxState;
     basis.push(1.0, auxState);
 
+    // go through all possible permutations
     while (true) {
         if (not increment(auxState)) {
             break;
--- src/state.hpp
+++ src/state.hpp
@@ -13,42 +13,54 @@
 #include "config.hpp"
 
 
+/// Indicate the presence of particles and holes at a lattice site.
 enum class PH : std::uint32_t
 {
-    n=0b00, p=0b01, h=0b10, ph=0b11
+    n=0b00,  ///< Neither particle or hole
+    p=0b01,  ///< A particle, no hole
+    h=0b10,  ///< A hole, no particle
+    ph=0b11  ///< Both particle and hole
 };
 
 
+/// Case a PH to its underlying type.
 constexpr auto underlying(PH const ph) noexcept
 {
     return static_cast<std::underlying_type_t<PH>>(ph);
 }
 
 
+/// Bit-wise and on PH.
 constexpr PH operator&(PH const a, PH const b) noexcept
 {
     return PH{underlying(a) & underlying(b)};
 }
 
 
+/// Bit-wise or on PH.
 constexpr PH operator|(PH const a, PH const b) noexcept
 {
     return PH{underlying(a) | underlying(b)};
 }
 
 
+/// Bit-wise 2's complement on PH.
 constexpr PH operator~(PH const ph) noexcept
 {
     return PH{~underlying(ph)};
 }
 
 
+/**
+ * Store PH values for all NSITES lattice sites.
+ */
 class State
 {
     std::array<PH, NSITES> sites_{};
 
 
 public:
+    /// Return `true` iff *all* sites are equal.
     constexpr bool operator==(State const &other) const noexcept
     {
         for (std::size_t i = 0; i < size(); ++i) {
@@ -60,12 +72,14 @@ public:
     }
 
 
+    /// Return number of alttice sites.
     [[nodiscard]] constexpr std::size_t size() const noexcept
     {
         return sites_.size();
     }
 
 
+    /// Access PH value at a site.
     constexpr PH &operator[](std::size_t const site) noexcept
     {
         assert(site < sites_.size());
@@ -73,6 +87,7 @@ public:
     }
 
 
+    /// Access PH value at a site.
     constexpr PH operator[](std::size_t const site) const noexcept
     {
         assert(site < sites_.size());
@@ -80,6 +95,7 @@ public:
     }
 
 
+    /// Return `true` if there is a particle on a site.
     [[nodiscard]] constexpr bool hasParticleOn(std::size_t const site) const noexcept
     {
         assert(site < sites_.size());
@@ -87,6 +103,7 @@ public:
     }
 
 
+    /// Return `true` if there is a heol on a site.
     [[nodiscard]] constexpr bool hasHoleOn(std::size_t const site) const noexcept
     {
         assert(site < sites_.size());
@@ -94,18 +111,22 @@ public:
     }
 
 
+    /// Return the number of particles+holes on a site.
     [[nodiscard]] constexpr int numberOn(std::size_t const site) const noexcept
     {
         assert(site < sites_.size());
 #if defined __GNUG__
+        // count number of set bits if possible
         static_assert(std::is_same_v<std::underlying_type_t<PH>, unsigned int>);
         return __builtin_popcount(underlying(sites_[site]));
 #else
+        // count manually (does not get optimised to popcount automatically)
         return static_cast<int>(hasParticleOn(site)) + static_cast<int>(hasHoleOn(site));
 #endif
     }
 
 
+    /// Make it so there is a particle at a site regardless of whether there was one already.
     constexpr void addParticleOn(std::size_t const site) noexcept
     {
         assert(site < sites_.size());
@@ -113,6 +134,7 @@ public:
     }
 
 
+    /// Make it so there is no particle at a site regardless of whether there was one in the first place.
     constexpr void removeParticleOn(std::size_t const site) noexcept
     {
         assert(site < sites_.size());
@@ -120,12 +142,14 @@ public:
     }
 
 
+    /// Make it so there is a hole at a site regardless of whether there was one already.
     constexpr void addHoleOn(std::size_t const site) noexcept {
         assert(site < sites_.size());
         sites_[site] = sites_[site] | PH::h;
     }
 
 
+    /// Make it so there is no hole at a site regardless of whether there was one in the first place.
     constexpr void removeHoleOn(std::size_t const site) noexcept {
         assert(site < sites_.size());
         sites_[site] = sites_[site] &~ PH::h;
@@ -133,24 +157,32 @@ public:
 };
 
 
+/// Return the number of sites based on a state.
 constexpr std::size_t size(State const &state) noexcept
 {
     return state.size();
 }
 
 
+/// Compute the dot product of two states.
 constexpr double dot(State const &a, State const &b) noexcept
 {
     return a == b ? +1.0 : 0.0;
 }
 
 
+/**
+ * Store multiple states and coefficients and represent them as their sum.
+ *
+ * Corresponds to sum_i sumState[i][0]*sumState[i][1].
+ */
 class SumState
 {
     std::vector<double> coefs_;
     std::vector<State> states_;
 
 public:
+    /// Reserve memory for n states.
     void reserve(std::size_t const n)
     {
         coefs_.reserve(n);
@@ -158,36 +190,42 @@ public:
     }
 
 
+    /// Return the number of currently stored states.
     [[nodiscard]] std::size_t size() const noexcept
     {
         return states_.size();
     }
 
 
+    /// Return coefficient and state number `i`.
     std::pair<double&, State&> operator[](std::size_t const i) noexcept
     {
         return {coefs_[i], states_[i]};
     }
 
 
+    /// Return coefficient and state number `i`.
     std::pair<double, State const&> operator[](std::size_t const i) const noexcept
     {
         return {coefs_[i], states_[i]};
     }
 
 
+    /// Access underlying state storage.
     [[nodiscard]] State const *states() const noexcept
     {
         return states_.data();
     }
 
 
+    /// Access underlying state storage.
     [[nodiscard]] State *states() noexcept
     {
         return states_.data();
     }
 
 
+    /// Append a new state.
     void push(double const coef, State const &state)
     {
         states_.emplace_back(state);
@@ -195,6 +233,7 @@ public:
     }
 
 
+    /// Remove all stored states and coefficients.
     void clear() noexcept
     {
         coefs_.clear();
@@ -202,10 +241,12 @@ public:
     }
 
 
+    /// Remove duplicate states by adding up their coefficients.
     void compress();
 };
 
 
+/// Compute the dot product of two SumStates.
 inline double dot(SumState const &a, SumState const &b) noexcept
 {
     double res = 0.0;
@@ -220,6 +261,11 @@ inline double dot(SumState const &a, SumState const &b) noexcept
 }
 
 
+/**
+ * Construct the basis of the fockspace.
+ * \return SumState containing basis vectors.
+ *         All coefficients are `1.0`.
+ */
 SumState fockspaceBasis();
 
 #endif //EXACT_HUBBARD_STATE_HPP
