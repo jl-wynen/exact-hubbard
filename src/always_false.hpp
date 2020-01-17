46e92dbe9d9a067b1158abc4f663982d8f7d5a0d
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-01-17 09:21:08 +0100

    Document alwaysFalse

--- src/always_false.hpp
+++ src/always_false.hpp
@@ -5,9 +5,14 @@
 #include <type_traits>
 
 
+/**
+ * A type that always represents `false` but defers evaluation using a template.
+ * \attention Do not specialise the template!
+ */
 template <typename>
 struct alwaysFalse : std::false_type {};
 
+/// Helper variable for alwaysFalse.
 template <typename T>
 constexpr static bool alwaysFalse_v = alwaysFalse<T>::value;
 
